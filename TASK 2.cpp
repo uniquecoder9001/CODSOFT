a4035ff1e6342f074c3ca2402fae87cc8e916931
uniquecoder9001 <145800675+uniquecoder9001@users.noreply.github.com>
2024-07-22 10:01:11 +0530

    Add files via upload

---
+++ TASK 2.cpp
@@ -0,0 +1,35 @@
+//      TASK 2 :-  SIMPLE CALCULATOR
+
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int a,b;
+    char op;
+    cout<<"Enter the value of First Operand:";
+    cin >> a;
+
+    cout<<"Enter the value of First Operand:";
+    cin >> a;
+
+    cout<<"Enter the Operation you want to perform:";
+    cin >> op;
+
+    switch(op){
+        case '+': cout<< "Addition of two Operand is : " << (a+b) <<endl;
+        break;
+
+        case '-': cout<< "Subtraction of two Operand is : " << (a-b) <<endl;
+        break;
+
+        case '*': cout<< "Muliplication of two Operand is : " << (a*b) <<endl;
+        break;
+
+        case '/': cout<< "Division of two Operand is : " << (a/b) <<endl;
+        break;
+
+        case '%': cout<< "Modular of two Operand is : " << (a%b) <<endl;
+        break;
+    }
+    return 0;
+}
